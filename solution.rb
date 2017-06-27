ac904614916d9c526537fa1f24eaf5f0b57d648b
Diego Camacho <hackvan@gmail.com>
2017-06-27 14:05:21 -0500

    adicionar ruta /makers

--- solution.rb
+++ solution.rb
@@ -1,8 +1,13 @@
 require 'sinatra'
+
 get '/' do
   unless nombre = params[:nombre]
     nombre = "desconocido"
   end
   nombre = "desconocido" if nombre.empty?
   "<h1>Hola #{nombre}</h1>"
+end
+
+get '/makers/:nombre' do
+  "<h1>Hola #{params[:nombre].capitalize}!</h1>"
 end
