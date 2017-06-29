d55478eb17ebdbb8fd6f7ac77f6ff2212848e7a4
Diego Camacho <hackvan@gmail.com>
2017-06-29 13:33:45 -0500

    Solución al reto Secret Code

--- solution.rb
+++ solution.rb
@@ -3,13 +3,3 @@ require 'sinatra'
 get '/' do
   erb :index
 end
-
-post '/saludar' do
-  @nombre = params[:nombre]
-  @nombre = "desconocido" if @nombre.empty?
-  erb :saludar
-end
-
-get '/makers/:nombre' do
-  "<h1>Hola #{params[:nombre].capitalize}!</h1>"
-end
