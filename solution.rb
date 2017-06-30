c34994a590d5baf34bfa9364bc27701fab4f40bc
Diego Camacho <hackvan@gmail.com>
2017-06-30 14:00:46 -0500

    Reto Sin permiso

--- solution.rb
+++ solution.rb
@@ -1,7 +1,12 @@
 require 'sinatra'
-$contador = 0
 
 get '/' do
-  @agente = request.user_agent
-  erb :index
+  token = request.env["HTTP_PERMISO"]
+  respuesta = "Sin Permiso"
+  if token == "soy-un-token-secreto"
+    respuesta = "Si lo logramos!"
+  end
+  respuesta
 end
+
+# curl -XGET -H "permiso: soy-un-token-secreto" "http://localhost:4567/"
